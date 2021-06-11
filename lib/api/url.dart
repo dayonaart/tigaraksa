11115e420a61ee9b4cb00b9b83a9964a38bdd311
dayonaart <54728700+dayonaart@users.noreply.github.com>
2021-06-11 17:46:28 +0700

    Update url.dart

--- lib/api/url.dart
+++ lib/api/url.dart
@@ -1,3 +1,3 @@
-const BASE_URL = "https://njapi.tigaraksa-ep.id:4000";
+const BASE_URL = "XXXXXXXX";
 const LOGIN_URL = "/api/and/users/ver/login";
 const REGISTER_URL = "/api/and/users/ver/new-customer";
